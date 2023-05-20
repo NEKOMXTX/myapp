7dc586750c3539675e459500f1574bec6096a137
nekomxtx <pockervlad@gmail.com>
2023-05-20 16:18:39 +0300

    end of lesson_12

--- server.js
+++ server.js
@@ -3,28 +3,30 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('mainpage'));
+    res.render(createPath('mainpage'));
 });
 
 app.get('/places', (req, res) => {
-    res.sendFile(createPath('places'));
+    res.render(createPath('places'));
 });
 
 app.get('/books', (req, res) => {
-    res.sendFile(createPath('books'));
+    res.render(createPath('books'));
 });
 
 app.use((req, res) => {
     res
         .status(404)
-        .sendFile(createPath('error'));
+        .render(createPath('error'));
 }); /* перехватчик ошибок должен быть в конце  */
